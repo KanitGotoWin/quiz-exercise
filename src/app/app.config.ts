b456ce4623d825e844c4bdabaafcc7bb79579e46
KanitGtw <kanit.gtw@gmail.com>
2025-03-01 14:34:21 +0700

    remove key and use .env instead

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,20 +6,14 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { provideHttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideClientHydration(),
     provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'angularquiz-84f99',
-        appId: '1:628097806306:web:33a976b3bf90c8c09a13bb',
-        storageBucket: 'angularquiz-84f99.firebasestorage.app',
-        apiKey: 'AIzaSyDHTeUv2IZBrvDeMLgOgMq0If8MWN6gjYA',
-        authDomain: 'angularquiz-84f99.firebaseapp.com',
-        messagingSenderId: '628097806306',
-      })
+      initializeApp(environment.firebase)
     ),
     provideFirestore(() => getFirestore()),
     provideHttpClient(),
